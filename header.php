01b5648f8087485189059689725cc5afd7e1441d
RafaRusso <54286057+RafaRusso@users.noreply.github.com>
2020-09-24 10:41:37 -0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<div class="navbar">
+	<img src="img/diagrama.png" width="3%">
+	<a href="index.php">Inicio</a>
+	<a href="news.php">Noticias</a>
+	<a href="contatos.php">Contatos</a>
+	<a href="registro.php" class="right">Registrar</a>
+</div>
