4ea9598a5a50f648468231c632d935e203bc0dbd
RafaRusso <54286057+RafaRusso@users.noreply.github.com>
2020-09-24 10:40:46 -0300

    Create contatos.php

---
+++ contatos.php
@@ -0,0 +1,15 @@
+<?php include("header.php"); 
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contatos - SGHI</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title></title>
+</head>
+<body>
+<?php include("header.php");?>
+</body>
+</html>
